9a1ebedc1f517a90c51429f3a9477545c9466c2c
Maciej Garncarski <maciejgarncarski@protonmail.com>
2026-01-10 17:10:38 +0100

    feat: improve help handler

--- backend/src/commands/help-command-handler.ts
+++ backend/src/commands/help-command-handler.ts
@@ -1,8 +1,39 @@
 import { CommandHandler, CommandContext } from "@/commands/command"
+import { env } from "@/config/env"
 import { RateLimitConfig } from "@/helpers/rate-limit"
+
+type CommandInfo = {
+  modOnly: boolean
+  text: string
+}
+
+type CommandsRecord = Record<string, CommandInfo>
 
 export class HelpCommandHandler extends CommandHandler {
   private readonly command = "help"
+  private readonly commands: CommandsRecord = {
+    sr: { modOnly: false, text: "<link | fraza>" },
+    song: { modOnly: false, text: "" },
+    queue: { modOnly: false, text: "" },
+    help: { modOnly: false, text: "" },
+    wrongsong: { modOnly: false, text: "" },
+    wrongsongall: { modOnly: true, text: "" },
+    github: { modOnly: false, text: "" },
+    voteskip: { modOnly: false, text: "" },
+    skip: { modOnly: false, text: "" },
+    next: { modOnly: false, text: "- informacje o nast." },
+    playlist: { modOnly: false, text: "<nazwa / link>" },
+    fill: { modOnly: false, text: "<fraza>" },
+    vanish: { modOnly: true, text: "" },
+    pause: { modOnly: true, text: "" },
+    play: { modOnly: true, text: "" },
+    volume: { modOnly: true, text: "<0-100>" },
+    seek: { modOnly: true, text: "<ss | mm:ss>" },
+    loop: { modOnly: true, text: "" },
+    remove: { modOnly: true, text: "<pozycja>" },
+    shuffle: { modOnly: true, text: "" },
+    clearall: { modOnly: true, text: "" },
+  }
 
   rateLimit: RateLimitConfig = {
     windowMs: 60000,
@@ -13,11 +44,18 @@ export class HelpCommandHandler extends CommandHandler {
     return command.toLowerCase() === this.command
   }
 
-  async execute({ deps: { logger, sendChatMessage }, messageId }: CommandContext) {
+  async execute({ deps: { logger, sendChatMessage }, isMod, messageId }: CommandContext) {
     logger.info(`[COMMAND] [HELP] Sending help message.`)
 
-    const helpMessage = `Dostępne komendy: !sr <link | fraza>, !song, !queue, !help - pokaż tę wiadomość, !wrongsong - usuń swoją piosenkę z kolejki, !wrongsongall, !github - link do repozytorium, !voteskip, !skip, !next - informacje o następnej piosence, !playlist <nazwa / link>, !fill <fraza>, !pause - (tylko mod), !play - (tylko mod), !volume <0-100> (tylko mod), !clearall (tylko mod).`
+    const filteredCommands = Object.fromEntries(
+      Object.entries(this.commands)
+        .sort(([a], [b]) => a.localeCompare(b))
+        .filter(([_, cmd]) => (cmd.modOnly ? isMod : true)),
+    )
 
+    const helpMessage = `Komendy: ${Object.entries(filteredCommands)
+      .map(([cmd, desc]) => `${env.COMMAND_PREFIX}${cmd} ${desc.text}`.trim())
+      .join(" | ")}`
     await sendChatMessage(helpMessage, messageId)
   }
 }
