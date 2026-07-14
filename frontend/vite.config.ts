336d33328de313b7640b4a24cc25dbba0a449dbe
Maciej Garncarski <maciejgarncarski@protonmail.com>
2026-07-14 20:21:38 +0200

    fix: port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,7 +16,7 @@ const dirname =
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3301,
     allowedHosts: true,
   },
   plugins: [
